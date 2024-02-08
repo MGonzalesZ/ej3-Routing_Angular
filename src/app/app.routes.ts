0d69974fa33e8a93f9c459a0a801d8560743a530
MGonzalesZ <mauriciogzb1322@gmail.com>
2024-02-08 12:44:19 -0400

    Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { NosotrosComponent } from './nosotros/nosotros.component';
+import { InscripcionesComponent } from './inscripciones/inscripciones.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'nosotros', component: NosotrosComponent },
+  { path: 'inscripciones', component: InscripcionesComponent },
+  { path: 'home', component: HomeComponent },
+  {
+    path: '**',
+    //   redirectTo: '/home'
+    component: NotfoundComponent,
+  },
+];
